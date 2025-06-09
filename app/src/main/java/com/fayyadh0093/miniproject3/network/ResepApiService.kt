0e48c0042589ad5a291b4c211abc9b400c4771f5
Muhammad Fayyadh Raihan <rehanmuhamad213@gmail.com>
2025-06-09 13:23:04 +0700

    membenahi kode dari warning dan ada warning yang tidak boleh dihapus.

--- app/src/main/java/com/fayyadh0093/miniproject3/network/ResepApiService.kt
+++ app/src/main/java/com/fayyadh0093/miniproject3/network/ResepApiService.kt
@@ -1,12 +1,8 @@
 package com.fayyadh0093.miniproject3.network
 
 import com.fayyadh0093.miniproject3.model.Resep
-import com.squareup.moshi.KotlinJsonAdapterFactory
-import com.squareup.moshi.Moshi
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
@@ -16,19 +12,6 @@ import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
 
-data class ResepUpdate(
-    val imageUrl: String
-)
-private const val BASE_URL = "https://6841688ed48516d1d35b74c4.mockapi.io"
-
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
-
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create(moshi))
-    .baseUrl(BASE_URL)
-    .build()
 
 interface ResepApiService {
     @GET("/Resep")
@@ -43,11 +26,6 @@ interface ResepApiService {
         @Path("id") id: String
     )
 
-    @PUT("/Resep/{id}")
-    suspend fun updateResep(
-        @Path("id") id: String,
-        @Body resep: Resep
-    ): Resep
 
     @FormUrlEncoded
     @PUT("/Resep/{id}")
@@ -71,11 +49,6 @@ interface ResepApiService {
         @Field("imageUrl") imageUrl: String
     ): Resep
 
-    @PUT("/Resep/{id}")
-    suspend fun updateResepImage(
-        @Path("id") id: String,
-        @Body update: ResepUpdate
-    )
 }
 
     object ResepApi {
